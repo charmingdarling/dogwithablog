bc1ffbdff633f518b9c439955da784e362954c98
charmingdarling <h1hasinai_f06@yahoo.com>
2023-10-15 12:36:29 -0700

    Added route to serialize data

--- controllers/model1-routes.js
+++ controllers/model1-routes.js
@@ -6,11 +6,23 @@ const router = require("express").Router();
 // The path module is particularly useful for dealing with file paths in a way that's agnostic to the underlying operating system. It helps ensure that your code works consistently across different platforms.
 const path = require("path");
 
+const Blog = require("./models/Blog");
+
 // Define routes using router.get(), router.post(), etc.
 // when a user accesses the root path ("/") of the application, this route handler will respond by sending the "index.html" file located in the "views" directory to the client's browser. This assumes that the file path is correctly specified, and the "index.html" file exists in the specified location.
 router.get("/", async (req, res) => {
   // here, the index.html is rendered to user
   res.sendFile(path.join(__dirname, "../views/index.html"));
+});
+
+// Building a route that serializes all the blog object that it receives.
+
+router.get("/", async (req, res) => {
+  const blogData = await Blog.findAll().catch((err) => {
+    res.json(err);
+  });
+  const blogs = blogData.map((blog) => blog.get({ plain: true }));
+  res.render("all", { blogs });
 });
 
 // router
