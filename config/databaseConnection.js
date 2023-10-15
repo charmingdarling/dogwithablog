38a1e530db7d4e2144a60bcdccb667be6ba0c530
charmingdarling <h1hasinai_f06@yahoo.com>
2023-10-15 01:45:46 -0700

    Router and path notes

--- config/databaseConnection.js
+++ config/databaseConnection.js
@@ -1,9 +1,12 @@
+// Importing Sequelize library, which is an ORM library for node designed to work with relational databases
+// Once you have imported Sequelize, you can use it to define models, interact with databases, perform CRUD operations, etc.
+
 const Sequelize = require("sequelize");
 
 // Loads all the env variables into the process and places it in process.env
 require("dotenv").config();
 
-// create a connection object
+// Creating a Sequelize instance and define a database connection
 const sequelize = new Sequelize(
   // database name
   process.env.DB_NAME,
@@ -14,6 +17,7 @@ const sequelize = new Sequelize(
   // database location
   {
     host: "localhost",
+    // or 'postgres', 'sqlite', etc
     dialect: "mysql",
     port: 3306,
   }
--- controllers/model1-routes.js
+++ controllers/model1-routes.js
@@ -1,10 +1,16 @@
+// Importing modules from Node.js using the CommonJS 'require' syntax
+// Importing the Router module from the express package. This class allows you to create modular, mountable route handlers. You can use it to define routes in separate fils and then bring them together in your main application file.
 const router = require("express").Router();
+
+// Imports the build-in `path` module, which provides utilities for working with the file and directory paths. This module is often used to construct or manipulate file paths in a cross platform manner
+// The path module is particularly useful for dealing with file paths in a way that's agnostic to the underlying operating system. It helps ensure that your code works consistently across different platforms.
 const path = require("path");
 
-// This is the `get` route
+// Define routes using router.get(), router.post(), etc.
 router.get("/", async (req, res) => {
   // here, the index.html is rendered
   res.sendFile(path.join(__dirname, "../views/index.html"));
 });
 
+// Export the router for use in other files
 module.exports = router;
