c7b2c8a632abf0c17177316be242d07c6cd195f2
charmingdarling <h1hasinai_f06@yahoo.com>
2023-10-15 22:26:58 -0700

    user, blog, post relationships

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 // Import your models from other files
 const Blog = require("./Blog");
+const User = require("./User");
 
 // Relationships are IDed here
 
@@ -7,4 +8,18 @@ const Blog = require("./Blog");
 // - hasMany
 // - belongsToMany
 
-module.exports = { Blog };
+//! Do I use 'user_id' ? or 'username_id' or ??
+
+User.hasMany(Blog, {
+  foreignKey: 'user_id',
+});
+
+Blog.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Blog.hasMany(Post, {
+  foreignKey: 
+});
+
+module.exports = { Blog, User };
