43c9ffec8b20c33c9bb8f580dd7dc8fbbc648d91
charmingdarling <h1hasinai_f06@yahoo.com>
2023-10-15 21:35:37 -0700

    Deleted const path and const Blog. Changed const Blog to correct pathing. Added an include to router.get /. Added { } around require Blog to import correctly. Corrected router.get/blog/:id to res.render(blogs...

--- controllers/blog-routes.js
+++ controllers/blog-routes.js
@@ -2,26 +2,16 @@
 // Importing the Router module from the express package. This class allows you to create modular, mountable route handlers. You can use it to define routes in separate fils and then bring them together in your main application file.
 const router = require("express").Router();
 
-// Imports the build-in `path` module, which provides utilities for working with the file and directory paths. This module is often used to construct or manipulate file paths in a cross platform manner
-// The path module is particularly useful for dealing with file paths in a way that's agnostic to the underlying operating system. It helps ensure that your code works consistently across different platforms.
-const path = require("path");
+// IMPORTANT: Must put these in {} because you're importing aliases
+const { Blog } = require("../models"); 
 
-const Blog = require("../models/Blog");
-
-// Define routes using router.get(), router.post(), etc.
-// when a user accesses the root path ("/") of the application, this route handler will respond by sending the "index.html" file located in the "views" directory to the client's browser. This assumes that the file path is correctly specified, and the "index.html" file exists in the specified location.
-// ! Do I even need lines 14-17?
+// GET ALL route, serializes blog object that is received
 router.get("/", async (req, res) => {
-  // here, the index.html is rendered to user
-  res.sendFile(path.join(__dirname, "../views/"));
-});
-
-// GET ALL route, serializes blog object that is receieved
-router.get("/", async (req, res) => {
-  const blogData = await Blog.findAll().catch((err) => {
-    res.json(err);
+  const blogData = await Blog.findAll({
+    include: [{ model: Blog }],
   });
   const blogs = blogData.map((blog) => blog.get({ plain: true }));
+  console.log(blogs);
   res.render("all", { blogs });
 });
 
@@ -34,7 +24,7 @@ router.get("/blog/:id", async (req, res) => {
       return;
     }
     const blog = blogData.get({ plain: true });
-    res.render("blog", blog);
+    res.render("blogs", blog);
   } catch (err) {
     res.status(500).json(err);
   }
